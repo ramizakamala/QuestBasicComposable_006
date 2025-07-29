699791bab71564652e8b392d32cf2f546c20fa38
Ramiza Kamala Tatsuru <mizarakamala@gmail.com>
2025-07-30 03:38:24 +0700

    menambahkan teks  "Nama" dengan jarak atas 5 dp sebagai penanda input pengguna pada tampilan LoginScreen

--- app/src/main/java/com/example/activity2/LoginScreen.kt
+++ app/src/main/java/com/example/activity2/LoginScreen.kt
@@ -56,4 +56,13 @@ fun LoginScreen() {
             modifier = Modifier.size(300.dp)
         )
 
+        Spacer(modifier = Modifier.height(5.dp))
+
+        Text(
+            text = "Nama",
+            fontSize = 16.sp,
+            fontWeight = FontWeight.SemiBold,
+            color = Color.Black
+        )
+
 }
