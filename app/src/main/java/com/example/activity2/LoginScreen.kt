77ff2b6a36f30f52591bccfff789ffc11f97d120
Ramiza Kamala Tatsuru <mizarakamala@gmail.com>
2025-07-30 03:42:21 +0700

    menambahkan komponen Surface berbentuk lingkaran berukuran 350 dp dengan border putih dan elevasi untuk elemen visual dekoratif pada LoginScreen

--- app/src/main/java/com/example/activity2/LoginScreen.kt
+++ app/src/main/java/com/example/activity2/LoginScreen.kt
@@ -80,5 +80,14 @@ fun LoginScreen() {
             color = Color.Black
         )
 
+        Spacer(modifier = Modifier.height(14.dp))
 
+        Surface(
+            shape = CircleShape,
+            modifier = Modifier
+                .size(350.dp)
+                .border(4.dp, Color.White, CircleShape),
+            tonalElevation = 6.dp
+        )
+    }
 }
